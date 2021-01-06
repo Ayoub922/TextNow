644dcc61c0a507c84bf8e69f1a441d89f5b71c21
Ayoub922 <60392992+Ayoub922@users.noreply.github.com>
2021-01-06 21:00:13 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/textit/AttachmentViewHolderImgur.kt
@@ -0,0 +1,34 @@
+package com.example.textit
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import coil.load
+import com.example.textit.databinding.ListItemAttachImgurBinding
+import com.getstream.sdk.chat.adapter.AttachmentListItem
+import com.getstream.sdk.chat.adapter.MessageListItem
+import com.getstream.sdk.chat.adapter.viewholder.attachment.BaseAttachmentViewHolder
+import com.getstream.sdk.chat.view.MessageListView
+
+class AttachmentViewHolderImgur(
+    parent: ViewGroup,
+    private val bubbleHelper: MessageListView.BubbleHelper,
+    private val messageItem: MessageListItem.MessageItem,
+    private val binding: ListItemAttachImgurBinding = ListItemAttachImgurBinding.inflate(
+        LayoutInflater.from(parent.context), parent, false
+    )
+) : BaseAttachmentViewHolder(binding.root) {
+
+    override fun bind(attachmentListItem: AttachmentListItem) {
+        val background = bubbleHelper.getDrawableForAttachment(
+            messageItem.message,
+            messageItem.isMine,
+            messageItem.positions,
+            attachmentListItem.attachment
+        )
+        binding.ivMediaThumb.setShape(context, background)
+
+        binding.ivMediaThumb.load(attachmentListItem.attachment.thumbUrl) {
+            allowHardware(false)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/textit/MyAttachmentViewHolderFactory.kt
@@ -0,0 +1,33 @@
+package com.example.textit
+
+import android.view.ViewGroup
+import com.getstream.sdk.chat.adapter.*
+import com.getstream.sdk.chat.adapter.viewholder.attachment.BaseAttachmentViewHolder
+import com.getstream.sdk.chat.view.MessageListViewStyle
+
+class MyAttachmentViewHolderFactory : AttachmentViewHolderFactory() {
+
+    override fun getAttachmentViewType(attachmentItem: AttachmentListItem): Int {
+        val imageUrl = attachmentItem.attachment.imageUrl ?: ""
+        return when {
+            imageUrl.contains("imgur") -> IMGUR_TYPE
+            else -> super.getAttachmentViewType(attachmentItem)
+        }
+    }
+
+    override fun createAttachmentViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+        style: MessageListViewStyle,
+        messageItem: MessageListItem.MessageItem
+    ): BaseAttachmentViewHolder {
+        return when (viewType) {
+            IMGUR_TYPE -> AttachmentViewHolderImgur(parent, bubbleHelper, messageItem)
+            else -> super.createAttachmentViewHolder(parent, viewType, style, messageItem)
+        }
+    }
+
+    companion object {
+        private const val IMGUR_TYPE = 0
+    }
+}
